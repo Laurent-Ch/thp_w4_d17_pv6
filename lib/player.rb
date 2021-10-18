dac5fb001f82f103b257ab31d608889aee97ac58
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-10-18 22:11:36 +0200

    now at 2.3.f., app.rb is complete

--- lib/player.rb
+++ lib/player.rb
@@ -30,3 +30,50 @@ class Player
   end
 
 end
+
+class HumanPlayer < Player
+  attr_accessor :weapon_level
+
+  def initialize(name_player)
+    @name = name_player
+    @life_points = 100
+    @weapon_level = 1
+  end
+
+  def show_state
+    puts "#{@name} a #{@life_points} points de vie et une arme de niveau #{@weapon_level}"
+  end
+
+  def compute_damage
+    rand(1..6) * @weapon_level
+  end
+
+  def search_weapon
+    random_weapon_barter = rand(1..6)
+    puts "tu as trouvé une arme de niveau #{random_weapon_barter}"
+    if random_weapon_barter > @weapon_level
+      @weapon_level = random_weapon_barter
+      puts "Youhou ! elle est meilleure que ton arme actuelle : tu la prends."
+    else
+      "M@*#$... elle n'est pas mieux que ton arme actuelle..."
+    end
+  end
+
+  def search_health_pack
+    random_health_pack = rand(1..6)
+    case 
+    when random_health_pack == 1
+      puts "Tu n'as rien trouvé..."
+    when random_health_pack == 6
+      puts "Waow, tu as trouvé un pack de +80 points de vie !"
+      @life_points += 80
+    else
+      puts "Bravo, tu as trouvé un pack de +50 points de vie !"
+      @life_points += 50
+    end
+    if @life_points > 100
+      @life_points = 100
+    end
+  end
+
+end
