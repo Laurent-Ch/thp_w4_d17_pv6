127f462604bbeb598ad781ba1dc541a95fe8915f
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-10-19 17:43:59 +0200

    acceptable version of menu-choice

--- app_3.rb
+++ app_3.rb
@@ -4,5 +4,33 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
+def continue_story
+  puts
+  print 'press any key to continue'
+  STDIN.getch
+  puts
+end
+
+puts "
+---------------------------------------------------
+|    WINNIE THE POO(H): RAMPAGE Edition —v2.0—    |
+| 'They took my honey... You'll take their lives' |
+---------------------------------------------------
+
+"
+=begin
+# Intro
+puts 'Bienvenue dans WTP:RAMPAGE. Quel est ton nom ?'
+print '>'
+player_name = gets.chomp
+puts
+my_game = Game.new(player_name)
+
+while my_game.is_still_ongoing? #while, mais pour le moment
+  puts my_game.show_players
+  puts my_game.menu
+end
+=end
+
 binding.pry
 
--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,5 @@
 NUMBER_OF_ENEMIES = 4
+
 class Game
   attr_accessor :human_player, :enemies
 
@@ -20,22 +21,53 @@ class Game
   def is_still_ongoing?
     @human_player.life_points > 0 && !@enemies.empty? ? true : false
   end
-  
+
   def show_players
     puts @human_player.show_state
     puts "il reste #{@enemies.size} bots."
   end
 
   def menu
-    puts 'Quelle action veux-tu effectuer ?'
+    puts "Quelle action veux-tu effectuer ? Tape a, s, ou le chiffre d'un ennemi encore présent."
     puts 'a - chercher une meilleure arme'
     puts 's - chercher à se soigner'
-    puts ()
+    puts
 
     puts 'attaquer un joueur en vue :'
+    counter = 0
     @enemies.each do |i|
-      puts '#{i} - #enemy1.show_state'
-      puts '1 - #enemy2.show_state'
+      print "#{counter} - "
+      puts i.show_state.to_s
+      counter += 1
+    end
   end
 
+  # This method ain't perfect: any other character than a or s is translated to 0, so Player0 gets attacked.
+  def menu_choice(user_input)
+    possible_string = user_input.to_s
+    possible_integer = user_input.to_i
+    if possible_string == 'a'
+      @human_player.search_weapon
+    elsif possible_string ==  's'
+      @human_player.search_health_pack
+    elsif possible_integer <= @enemies.size - 1
+      human_player.attacks(@enemies.at(possible_integer))
+    else
+      puts 'please enter a, s, or the number of a remaining enemy'
+    end
+  end
+
+  def enemies_attack
+    @enemies.each do |i|
+      i.attacks(@human_player)
+    end
+  end
+
+  def end
+    if @human_player.life_points > 0
+      puts "Bravo #{@human_player.name}, je vais pouvoir hiberner en paix cet hiver. Prends donc ces lingots d'or inutiles, pâles imitations de mon miel adoré."
+    else
+      puts "Tu as échoué. Je maigris à vue d'oeil... mes forces s'amoindrissent ; c'est... bientôt la fin."
+    end
+  end
 end
