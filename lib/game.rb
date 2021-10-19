fe4fe8891358ff6b7dffae86d45c7e33d716c229
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-10-19 14:46:03 +0200

    is_still_ongoing? method works

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,24 @@
 NUMBER_OF_ENEMIES = 4
 class Game
   attr_accessor :human_player, :enemies
-  
+
   def initialize(character_name)
     @human_player = HumanPlayer.new(character_name)
     @enemies = []
     NUMBER_OF_ENEMIES.times do |i|
-      @enemies << "Player#{i}"
+      @enemies << Player.new("Player#{i}")
     end
-    puts enemies
   end
 
   # En travaux
-  def kill_player(dead_player)
-    enemies.delete(dead_player)
+  def kill_player(player_to_kill)
+    @enemies.each do |i|
+      @enemies.delete(i) if i.name == player_to_kill
+    end
   end
 
-
+  def is_still_ongoing?
+    @human_player.life_points > 0 && !@enemies.empty? ? true : false
+  end
+  
 end
